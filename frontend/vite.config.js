b09b5a475e6d7d4e45b7d2bd64f616bba5ee5903
Rounik Mondal <rounikmondal74@gmail.com>
2026-04-22 11:44:52 +0530

    feat: vercel fixes:

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-   resolve: {
+  base: "/",
+  plugins: [react(), tailwindcss()],
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
@@ -17,4 +16,4 @@ export default defineConfig({
       "/api": "http://localhost:3001"
     }
   }
-})
+});
